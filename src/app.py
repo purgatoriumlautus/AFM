2594717393716396f04f3f9aed00b2c30a48b35c
Alisa Beztsinna <23imc10600@fh-krems.ac.at>
2024-11-03 21:12:08 +0100

    Main function added

--- src/app.py
+++ src/app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, jsonify
 # from user import users
+from user import user
 
 app = Flask(__name__)
 
 # USERS DUMMY DATA
 users = [{"name":"John","surname":"Doe","email":"123@gmail.com","password":"12345678"}] #dummy data
-
+app.register_blueprint(user)
 @app.route('/')
-def index():
-    return render_template('index.html')
+def main():
+    return render_template('mainpage.html')
 @app.route('/users')
 def get_users():
     return render_template('users.html',users=users)
