247cca7d7117265a776888464f220bb4f70eed03
Alisa Beztsinna <23imc10600@fh-krems.ac.at>
2024-11-12 23:25:56 +0100

    Report page added with geolocation, description and photo; Report model in database, page with all reports and some small changes

--- src/app.py
+++ src/app.py
@@ -1,16 +1,20 @@
 import requests
-from flask import Flask, render_template, jsonify, url_for, make_response, request, session
+from flask import Flask, render_template, jsonify, url_for, make_response, request, session, redirect
 from flask_login import UserMixin
 # from src import create_app
+from src.db import db, User, Report
 from src.userAPI import user, register_user, login_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] ="secretkey"
-db = SQLAlchemy(app)
+app.config['UPLOAD_FOLDER'] = 'src/static'
+db.init_app(app)
 
 
 # app.register_blueprint(user)
@@ -36,6 +40,31 @@ def login():
 @app.route("/register")
 def register():
     return render_template('register.html')
+@app.route("/report")
+def report():
+    return render_template('report.html')
+@app.route('/create-report', methods = ['POST'] )
+def create_report():
+    location = request.form.get('location')
+    description = request.form.get('description')
+    photo = request.files.get('photo')
+
+    if photo:
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    else:
+        filename = None
+
+    report = Report(location, description, filename)
+    db.session.add(report)
+    db.session.commit()
+    return redirect(url_for('main'))
+@app.route('/view-reports')
+def view_reports():
+    reports = Report.query.all()
+    return render_template('reports.html', reports=reports)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/db.py
+++ src/db.py
@@ -14,9 +14,16 @@ class User(db.Model): #db table for users should be modified in future
     password = db.Column(db.Text())
     registred_at = db.Column(db.DateTime,nullable=False,default=datetime.now())
     updated_at = db.Column(db.DateTime,onupdate=datetime.now(),nullable=True)
-
-
     def __repr__(self):
         return f"User>>> {self.name}"
-    
+class Report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    photo_file = db.Column(db.String(100), nullable=True)
+
+    def __init__(self, location, description, photo_file = None):
+        self.location = location
+        self.description = description
+        self.photo_file = photo_file
 
