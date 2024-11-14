5b70e2ae0830f1baa3829f234d89eac6af11b1d3
AlekkDev <22imc10240@fh-krems.ac.at>
2024-11-14 17:21:47 +0100

    Authentication functionality added
    Users can register, login and logout but some redirection functionality missing

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,18 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from src.models import User
 from flask_login import login_user, current_user, logout_user
+
+
+# REGISTER ROUTES
+
+
+
 def register_routes(app,db,bcrypt):
     # REGISTER ROUTES
     @app.route('/', methods=['GET', 'POST'])
-    def index():
+    def mainpage():
         if current_user.is_authenticated:
+            # IF USER IS LOGGED IN
             return render_template('mainpage.html')
         else:
             return render_template('mainpage.html')
@@ -22,23 +29,40 @@ def register_routes(app,db,bcrypt):
     def get_users():
         return render_template('users.html')
 
-    @app.route('/login')
-    def show_login():
-        return render_template('login.html')
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
 
-    @app.route('/login/<user_id>')
-    def login(user_id):
-        user = User.query.get(user_id)
-        if user:
-            login_user(user)
-            return "Success"
-        else:
-            return "User not found"
+
+            user = User.query.filter(User.username==username).first()
+            if bcrypt.check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('mainpage'))
+            else:
+                return "Login Failed"
     @app.route('/logout')
     def logout():
         logout_user()
         return "Success"
 
-    @app.route("/register")
+    @app.route("/register", methods=['GET', 'POST'])
+    def register():
+        if request.method == 'GET':
+            return render_template('register.html')
+        elif request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            email = request.form['email']
+
+            hashed_password = bcrypt.generate_password_hash(password)
+
+            user = User(username=username, password=hashed_password, email=email)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('mainpage'))
     def register():
         return render_template('register.html')
