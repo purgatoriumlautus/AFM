2b3bf7b6a94a15831025a9a7da782b22910cc87f
Alisa Beztsinna <23imc10600@fh-krems.ac.at>
2025-01-02 21:18:29 +0100

    Dashboard for managers and agents added

--- src/routes/main.py
+++ src/routes/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user,current_user, login_required
-from src.models import User,Report,Organisation
+from src.models import User,Report,Organisation, Manager, Agent
 from src.extensions import bcrypt
 from src.db import db
 import os
@@ -46,3 +46,35 @@ def join_organization(token):
 
     flash(f"You have successfully joined the organization '{organisation.name}'.", "success")
     return redirect(url_for('main.mainpage',is_super_admin=is_super_admin()))
+
+@main.route('/dashboard', methods=['GET'])
+@login_required
+def see_dashboard():
+    role = None
+    task_data = []
+
+    manager = Manager.query.filter_by(user_id=current_user.uid).first()
+    if manager:
+        role = "Manager"
+        task_data = [{
+            'title': task.name,
+            'location': task.reports[0].location if task.reports else "0,0",
+            'description': task.description,
+            'status': task.status
+        } for task in manager.tasks]
+
+    agent = Agent.query.filter_by(user_id=current_user.uid).first()
+    if agent:
+        role = "Agent"
+        task_data = [{
+            'title': task.name,
+            'location': task.reports[0].location if task.reports else "0,0",
+            'description': task.description,
+            'status': task.status
+        } for task in agent.tasks]
+
+    if role:
+        return render_template("dashboard.html", tasks=task_data, user=current_user, role=role)
+
+    flash("You are not authorized to view this page.", "danger")
+    return redirect(url_for('main.mainpage'))
